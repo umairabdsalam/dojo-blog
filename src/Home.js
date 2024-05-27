cb84c90a002ca27d503c1a8e434f86b9145c817c
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-05-27 13:00:40 +0100

    updatesJsonServer

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+// import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+
+
+
+
+
+
+
+
+    return ( 
+        <div className="home">
+
+            { isPending && <div>Loading..................................................</div> }
+            { error && <div> { error } </div> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs"></BlogList> }
+            { blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"></BlogList>}
+        </div>
+     );
+}
+ 
+export default Home;
